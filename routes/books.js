51384b483bdcbb6f2769eb3f0413cd48b7e014ca
koushik717 <101172701+koushik717@users.noreply.github.com>
2024-09-18 12:28:42 -0400

    Create books.js

---
+++ routes/books.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/Book');
+
+// Get all books
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create a new book
+router.post('/', async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    publishedDate: req.body.publishedDate
+  });
+
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Update a book
+router.patch('/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (req.body.title) book.title = req.body.title;
+    if (req.body.author) book.author = req.body.author;
+    if (req.body.genre) book.genre = req.body.genre;
+    if (req.body.publishedDate) book.publishedDate = req.body.publishedDate;
+
+    const updatedBook = await book.save();
+    res.json(updatedBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a book
+router.delete('/:id', async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Book deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
